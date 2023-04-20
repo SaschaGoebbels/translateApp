7dc1659bc2c77dd08e67a9c7a30f51aa0165a5c4
SaschaGoebbels <109481671+SaschaGo2003@users.noreply.github.com>
2023-04-20 18:41:44 +0200

    update

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import and from '../and.svg';
+import classes from './header.module.css';
+
+const Header = props => {
+  //
+  return (
+    <header className={classes.header}>
+      <div className={classes.headline}>
+        <h1>translate</h1>
+        <img src={and} className={classes.andSymbol} alt="&" />
+        <h1>repeat</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
