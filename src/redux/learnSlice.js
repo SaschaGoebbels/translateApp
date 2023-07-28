8e30cf6f1d61f7bfee346f2b31f310d43a22977f
SaschaGoebbels <109481671+SaschaGo2003@users.noreply.github.com>
2023-07-28 22:40:57 +0200

    data

--- src/redux/learnSlice.js
+++ src/redux/learnSlice.js
@@ -8,52 +8,13 @@ import {
   readLocalStorageByKey,
 } from '../store/localStorage';
 
-const exampleList = [
-  {
-    text1: 'was machen wir heute',
-    language1: 'en',
-    text2: 'What shall we do today',
-    language2: 'de',
-    interval: 3,
-    count: 0,
-    id: '1fea35ea-7aea-d466-7a4f-9c260cf365af',
-    fav: false,
-    timestamp: 1683797600963,
-  },
-  {
-    text1: 'wie geht es dir',
-    language1: 'en',
-    text2: 'How are you',
-    language2: 'de',
-    interval: 2,
-    count: 0,
-    id: '399120de-e534-72b7-e3a2-3a1aa9b11b62',
-    fav: false,
-    timestamp: 1683797593211,
-  },
-  {
-    text1: 'welt',
-    language1: 'en',
-    text2: 'world',
-    language2: 'de',
-    interval: 1,
-    count: 0,
-    id: 'bbc1720e-7a78-b5fc-237f-a8bab7e716a3',
-    fav: false,
-    timestamp: 1683797435524,
-  },
-  {
-    text1: 'hallo',
-    language1: 'en',
-    text2: 'Hello',
-    language2: 'de',
-    interval: 1,
-    count: 0,
-    id: '4d781420-2f99-66b2-b5d9-ecc17401302f',
-    fav: false,
-    timestamp: 1683797430376,
-  },
-];
+//==================================================================
+import data from '../files/voca.json';
+const exList = data.learn.learn.current.list;
+console.log('✅', exList);
+//==================================================================
+
+const exampleList = exList;
 
 const initialState = {
   learn: {
@@ -213,3 +174,50 @@ export const {
   learnStateLocalData,
 } = learnSlice.actions;
 export default learnSlice.reducer;
+
+// [
+//   {
+//     text1: 'was machen wir heute',
+//     language1: 'en',
+//     text2: 'What shall we do today',
+//     language2: 'de',
+//     interval: 3,
+//     count: 0,
+//     id: '1fea35ea-7aea-d466-7a4f-9c260cf365af',
+//     fav: false,
+//     timestamp: 1683797600963,
+//   },
+//   {
+//     text1: 'wie geht es dir',
+//     language1: 'en',
+//     text2: 'How are you',
+//     language2: 'de',
+//     interval: 2,
+//     count: 0,
+//     id: '399120de-e534-72b7-e3a2-3a1aa9b11b62',
+//     fav: false,
+//     timestamp: 1683797593211,
+//   },
+//   {
+//     text1: 'welt',
+//     language1: 'en',
+//     text2: 'world',
+//     language2: 'de',
+//     interval: 1,
+//     count: 0,
+//     id: 'bbc1720e-7a78-b5fc-237f-a8bab7e716a3',
+//     fav: false,
+//     timestamp: 1683797435524,
+//   },
+//   {
+//     text1: 'hallo',
+//     language1: 'en',
+//     text2: 'Hello',
+//     language2: 'de',
+//     interval: 1,
+//     count: 0,
+//     id: '4d781420-2f99-66b2-b5d9-ecc17401302f',
+//     fav: false,
+//     timestamp: 1683797430376,
+//   },
+// ];
